694a672307c4cde56a15df792f6b7d9b83942356
shaozhenya <s88310301@163.com>
2025-03-13 16:44:54 +0800

    测试测试11

--- caffeine-test/src/main/java/facade/Test.java
+++ caffeine-test/src/main/java/facade/Test.java
@@ -1,11 +1,34 @@
 package facade;
 
+import com.alibaba.fastjson.JSONObject;
+import lombok.*;
+
 /**
  * @author shaozhenya
  * @date 2025/03/10
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Test {
+    public static final String a = "12312";
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 名字
+     */
+    private String name;
+
     public static void main(String[] args) {
-        System.out.println(111);
+        Test test = new Test();
+        test.setAge(1);
+        test.setName("你好");
+
+        System.out.println(JSONObject.toJSON(test));
     }
 }
